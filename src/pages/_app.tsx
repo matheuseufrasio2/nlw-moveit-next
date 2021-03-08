1c589b4a6aaeab9a548d2bfe689feb2276069af8
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-03-08 15:37:16 -0300

    aula 3 terminada - adicionando contexto a aplicaçao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/global.css'
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+    
+  )
 }
 
 export default MyApp
